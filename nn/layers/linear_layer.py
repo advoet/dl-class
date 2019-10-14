573171ccaf3772cd25f1a8cc2b756e1385db2d35
Alex Voet <advoet@gmail.com>
2019-10-13 17:35:20 -0700

    hw1 sync

--- nn/layers/linear_layer.py
+++ nn/layers/linear_layer.py
@@ -10,26 +10,39 @@ class LinearLayer(Layer):
     def __init__(self, input_size: int, output_size: int, parent=None):
         super(LinearLayer, self).__init__(parent)
         self.bias = Parameter(np.zeros((1, output_size), dtype=np.float32))
-        self.weight = ??? # TODO create the weight parameter
+        self.weight = Parameter(np.ones((input_size, output_size),dtype=np.float32))
+        self.input = None
+        self.input_size = input_size
+        self.output_size = output_size
         self.initialize()
 
-    def forward(self, data: np.ndarray) -> np.ndarray:
-        """
+    def forward(self, data: np.ndarray): # -> np.ndarray:
+        '''
         Linear layer (fully connected) forward pass
         :param data: n X d array (batch x features)
         :return: n X c array (batch x channels)
-        """
+        '''
         # TODO do the linear layer
-        return None
+        self.input = Parameter(data)
+        return np.matmul(data, self.weight.data) + self.bias.data
 
-    def backward(self, previous_partial_gradient: np.ndarray) -> np.ndarray:
+    def backward(self, previous_partial_gradient: np.ndarray): #-> np.ndarray
         """
         Does the backwards computation of gradients wrt weights and inputs
         :param previous_partial_gradient: n X c partial gradients wrt future layer
         :return: gradients wrt inputs
         """
-        # TODO do the backward step
-        return None
+        # dL/db = dL/dy but how to deal with batches??? ANS: SUM FOR SOME REASON
+        # dL/dw_ij = dL/dyj dy/dwij = dL/dyj * xi
+        self.bias.grad = np.sum(previous_partial_gradient, axis = 0)
+
+        batch_w_grad = np.zeros((self.input_size, self.output_size, np.size(previous_partial_gradient, 0)))
+        for i in range(0, self.input_size):
+            for j in range(0, self.output_size):
+                batch_w_grad[i,j,:] = np.multiply(previous_partial_gradient[:,j], self.input.data[:,i])
+        self.weight.grad = np.sum(batch_w_grad, axis = 2)
+        #self.weight.grad = np.mean(batch_w_grad, axis = 2)
+        return np.matmul(previous_partial_gradient, self.weight.data.T)
 
     def selfstr(self):
         return str(self.weight.data.shape)
--- nn/layers/losses/softmax_cross_entropy_loss_layer.py
+++ nn/layers/losses/softmax_cross_entropy_loss_layer.py
@@ -11,23 +11,44 @@ class SoftmaxCrossEntropyLossLayer(LossLayer):
             sum reduction means the results should be summed.
         """
         self.reduction = reduction
+        self.softmax = None
+        self.targets = None
         super(SoftmaxCrossEntropyLossLayer, self).__init__(parent)
 
-    def forward(self, logits, targets, axis=-1) -> float:
+    def forward(self, logits, targets, axis=-1): #-> float:
         """
 
         :param logits: ND non-softmaxed outputs. All dimensions (after removing the "axis" dimension) should have the same length as targets
-        :param targets: (N-1)D class id integers.
+            2D - (batch) x (class)
+        :param targets: (N-1)D class id intege
+            1D - (batch)
         :param axis: Dimension over which to run the Softmax and compare labels.
         :return: single float of the loss.
         """
-        # TODO
-        return 0
+        self.targets = targets
 
-    def backward(self) -> np.ndarray:
+        normalized_logits = logits - logits.max(axis = axis, keepdims = True)
+        sum_exp_logits = np.sum(np.exp(normalized_logits), axis = axis, keepdims = True)
+        self.softmax = np.exp(normalized_logits)/sum_exp_logits
+        log_softmax =  normalized_logits - np.log(sum_exp_logits)
+
+        # targets[k] = integer representing the index of the actual class for kth trial in batch
+        # this has the effect of grabbing [k, targets[k]]
+        cross_entropy = -log_softmax[np.arange(len(targets)), targets]
+
+        if (self.reduction == "mean"):
+            return np.mean(cross_entropy)
+        elif (self.reduction == "sum"):
+            return np.sum(cross_entropy)
+        else:
+            return 0
+
+
+    def backward(self): # -> np.ndarray:
         """
         Takes no inputs (should reuse computation from the forward pass)
         :return: gradients wrt the logits the same shape as the input logits
         """
-        # TODO
-        return None
+        gradients = (1/20.)*self.softmax.copy() #multiply this by scaling factor
+        gradients[np.arange(len(self.targets)), self.targets] -= 1
+        return gradients
