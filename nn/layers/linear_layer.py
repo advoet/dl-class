corrected formula in cross entropy layer

nn/layers/linear_layer.py
@@ -7,6 +7,9 @@ from .layer import Layer
 
 
 class LinearLayer(Layer):
+
+    iters = 0
+
     def __init__(self, input_size: int, output_size: int, parent=None):
         super(LinearLayer, self).__init__(parent)
         self.bias = Parameter(np.zeros((1, output_size), dtype=np.float32))
@@ -24,7 +27,16 @@ class LinearLayer(Layer):
         '''
         # TODO do the linear layer
         self.input = Parameter(data)
-        return np.matmul(data, self.weight.data) + self.bias.data
+        #-
+        #LinearLayer.iters+=1
+        #print(LinearLayer.iters)
+        #print(self.weight.grad)
+        #
+        #ha! my weights were exploding so I used regularization, but it didnt work T_T
+        #
+        #self.input = Parameter(data.astype(np.float64))
+        #
+        return np.matmul(self.input.data, self.weight.data) + self.bias.data
 
     def backward(self, previous_partial_gradient: np.ndarray): #-> np.ndarray
         """
@@ -32,17 +44,11 @@ class LinearLayer(Layer):
         :param previous_partial_gradient: n X c partial gradients wrt future layer
         :return: gradients wrt inputs
         """
-        # dL/db = dL/dy but how to deal with batches??? ANS: SUM FOR SOME REASON
-        # dL/dw_ij = dL/dyj dy/dwij = dL/dyj * xi
+        
         self.bias.grad = np.sum(previous_partial_gradient, axis = 0)
-
-        batch_w_grad = np.zeros((self.input_size, self.output_size, np.size(previous_partial_gradient, 0)))
-        for i in range(0, self.input_size):
-            for j in range(0, self.output_size):
-                batch_w_grad[i,j,:] = np.multiply(previous_partial_gradient[:,j], self.input.data[:,i])
-        self.weight.grad = np.sum(batch_w_grad, axis = 2)
-        #self.weight.grad = np.mean(batch_w_grad, axis = 2)
-        return np.matmul(previous_partial_gradient, self.weight.data.T)
+        self.weight.grad = np.matmul(self.input.data.T, previous_partial_gradient)
+        
+        return np.matmul(previous_partial_gradient, self.weight.data.T, dtype = np.double)
 
     def selfstr(self):
         return str(self.weight.data.shape)

nn/layers/losses/softmax_cross_entropy_loss_layer.py
@@ -49,6 +49,10 @@ class SoftmaxCrossEntropyLossLayer(LossLayer):
         Takes no inputs (should reuse computation from the forward pass)
         :return: gradients wrt the logits the same shape as the input logits
         """
-        gradients = (1/20.)*self.softmax.copy() #multiply this by scaling factor
+
+        gradients = self.softmax.copy()
         gradients[np.arange(len(self.targets)), self.targets] -= 1
+        if self.reduction is "mean":
+            gradients *= (1/np.size(self.softmax, 0)) #multiply this by scaling factor
+        
         return gradients

nn/layers/relu_layer.py
@@ -2,18 +2,13 @@ import numpy as np
 from numba import njit, prange
 
 from .layer import Layer
-from .linear_layer import LinearLayer
+from nn import Parameter
 
 
-class ReLULayer(LinearLayer):
-    def __init__(self, input_size: int, output_size: int, parent=None):
+class ReLULayer(Layer):
+    def __init__(self, parent=None):
         super(ReLULayer, self).__init__(parent)
-        self.bias = Parameter(np.zeros((1, output_size), dtype=np.float32))
-        self.weight = Parameter(np.ones((input_size, output_size),dtype=np.float32))
-        self.input = None
-        self.forward_linear_data = None
-        self.input_size = input_size
-        self.output_size = output_size
+        self.data = None
         self.initialize()
 
     def forward(self, data):
@@ -21,10 +16,10 @@ class ReLULayer(LinearLayer):
         ReLU Layer forward pass
 
         :param data: n x d (batch x features)
-        :return: n x c array (batch x channels) 
+        :return: n x d array relu activated (batch x channels) 
         '''
-        self.forward_linear_data = Parameter(super().forward(data))
-        return relu_activation(forward_linear_data.data)
+        self.data = Parameter(data)
+        return np.clip(data, 0, None)
 
     def backward(self, previous_partial_gradient):
         '''
@@ -33,22 +28,7 @@ class ReLULayer(LinearLayer):
         :param previous_partial_gradient: n x c of gradients w.r.t. future layer
         :return: gradients w.r.t. inputs
         '''
-        online_bias_grad = previous_partial_gradient * (self.forward_linear_data.data > 0)
-        self.bias.grad = np.sum(online_bias_grad, axis = 0)
-
-        batch_w_grad = np.zeros((self.input_size, self.output_size, np.size(previous_partial_gradient, 0)))
-        for i in range(0, self.input_size):
-            for j in range(0, self.output_size):
-                batch_w_grad[i,j,:] = np.multiply(previous_partial_gradient[:,j], self.input.data[:,i]) * (self.forward_linear_data.data[:,j] > 0)
-        self.weight.grad = np.sum(batch_w_grad, axis = 2)
-
-
-        return np.matmul(previous_partial_gradient * (self.forward_linear_data.data > 0), self.weight.data.T)
-
-    def relu_activation(forward_linear_data):
-        relu_activated = forward_linear_data.copy()
-        relu_activated *= forward_linear_data > 0
-        return relu_activated
+        return np.where(self.data.data > 0, previous_partial_gradient, 0)
 
 class ReLUNumbaLayer(Layer):
     def __init__(self, parent=None):

test/hw1_tests/test_softmax_cross_entropy_loss_layer.py
@@ -93,8 +93,10 @@ def _test_backward(input_shape, reduction, axis):
     if axis != 1:
         torch_grad = np.moveaxis(torch_grad, 1, axis)
 
+    print(torch_grad.shape)
+    print(grad.shape)
     print("grad - torch_grad < .000001")
-    print(grad - torch_grad < .000001)
+    print(np.absolute(grad - torch_grad) < .9)
 
     assert np.allclose(grad, torch_grad, atol=0.001)
 