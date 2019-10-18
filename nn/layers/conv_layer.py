c628305c575318b613bd1f6810db05ba7b097948
Alex Voet <advoet@gmail.com>
2019-10-17 18:31:45 -0700

    flatten layer, conv attempt

--- nn/layers/conv_layer.py
+++ nn/layers/conv_layer.py
@@ -9,12 +9,21 @@ from .layer import Layer
 
 class ConvLayer(Layer):
     def __init__(self, input_channels, output_channels, kernel_size=3, stride=1, parent=None):
+        '''
+        Each channel corresponds to a filter. The first layer has 1 input channel (the original image) and we
+        apply a # of filters = to # of output channels.
+
+        '''
         super(ConvLayer, self).__init__(parent)
         self.weight = Parameter(np.zeros((input_channels, output_channels, kernel_size, kernel_size), dtype=np.float32))
         self.bias = Parameter(np.zeros(output_channels, dtype=np.float32))
+        
+        self.input_channels = input_channels
+        self.output_channels = output_channels
         self.kernel_size = kernel_size
         self.padding = (kernel_size - 1) // 2
         self.stride = stride
+        self.im2col = None
         self.initialize()
 
     @staticmethod
@@ -22,13 +31,33 @@ class ConvLayer(Layer):
     def forward_numba(data, weights, bias):
         # TODO
         # data is N (batch) x C (input channels) x H(eight) x W(idth)
-        # kernel is COld x CNew x K x K
-        return None
+        # kernel is COld x CNew x K (size) x K (size)
+        for i in prange(data.shape[0]):
+            self.im2col[i,:,:] = self.im_2_col(data, i)
 
     def forward(self, data):
-        # TODO
+        self.im2col = np.zeros((np.size(data,0)), self.kernel_size*self.kernel_size*self.input_channels, np.size(data,2)*np.size(data,3), dtype = np.float32)
+        self.im_2_col(data)
+        self.filters
+        return 
 
-        return None
+    @njit(parallel = True, cache = True)
+    def im_2_col(self, data, index):
+        '''
+        Processes data into im2col form from lecture slides.
+         - Data is not padded to begin with, so pad during conversion
+         - Take into account stride as well
+         - Each square analyzed by the filter becomes a column
+         - In this manner, one channel (input image) is processed into a block row consisting of these columns
+
+        final size is:
+         (area of filter * channels) x (number of pixels in image)
+
+         reshapes image in data[index, :, :, :]
+         places in self.im2col[index,:,:]
+        '''
+        
+    #DO WE NEED A COL_2_IM AS WELL? PROBABLY
 
     @staticmethod
     @njit(cache=True, parallel=True)
