8af8d1f7c11658f1c030d7924f34f47deb8ab201
Alex Voet <advoet@gmail.com>
2019-11-01 16:02:44 -0700

    parallel im2col

--- nn/layers/conv_layer.py
+++ nn/layers/conv_layer.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from numba import jit,njit, prange
 
+from itertools import product
+
 from nn import Parameter
 from .layer import Layer
 
@@ -55,38 +57,97 @@ class ConvLayer(Layer):
 
     @staticmethod
     @njit(parallel=True, cache=True)
-    def numba_im_2_col(data,kernel_size,padding,height,width,X_col):
-        def get_square(row, col):
-            # gets the data from the square in data with top left corner (row, col)
-            # deals with the padding. Probably faster to just use numpy pad
-            square = np.zeros((kernel_size, kernel_size))
-            '''
-            for i_square, i in enumerate(range(row, row+kernel_size)):
-                for j_square, j in enumerate(range(col, col + kernel_size)):
-                    if i < padding or i >= padding + data.shape[2] or j < padding or j >= padding + data.shape[3]:
-                        square[i_square,j_square] = 0
+    def numba_im_2_col(X, H0, H1, W0, W1, D0, D1, P, K, S):
+        '''
+        trying to parallelize
+        See im_2_col_no_sq
+        '''
+        X_col = np.zeros((X.shape[0], K*K*D0, H1*W1), dtype=np.float32)
+        
+
+        for l in prange(D0*K*K*H1*W1):
+            # p gives the channel
+            p = l // (H1*W1)
+            # q gives the location
+            q = l % (H1*W1)
+            # d0 is the row in X_col
+            d0 = (p//K)//K
+            i0 = S*(q//W1) + (p//K) %K
+            j0 = S*(q % W1) + p % K
+            for n in prange(X.shape[0]):
+                if (i0 >= P and j0 >= P and i0 < H0 + P and j0 < W0 + P):
+                    X_col[n,p,q] = X[n,d0,i0-P,j0-P]
+                else:
+                    X_col[n,p,q] = 0
+        return X_col
+
+    
+    @staticmethod
+    @njit(parallel=True, cache=True)
+    def numba_im_2_col_2(X, H0, H1, W0, W1, D0, D1, P, K, S):
+        '''
+        trying to parallelize
+        See im_2_col_no_sq
+        '''
+        X_col = np.zeros((X.shape[0], K*K*D0, H1*W1), dtype=np.float32)
+        
+
+        for loc in prange(H1*W1):
+            tl_r = S*(loc // W1)
+            tl_c = S*(loc % W1)
+            for row in prange(K*K*D0):
+                d0 = row//(K*K)
+                r0 = row % (K*K)
+                i0 = tl_r + (r0 // K)
+                j0 = tl_c + (r0 % K)
+                
+                for n in prange(X.shape[0]):
+                    if (i0 >= P and j0 >= P and i0 < H0 + P and j0 < W0 + P):
+                        X_col[n,row,loc] = X[n,d0,i0-P,j0-P]
                     else:
-                        square[i_square,j_square] = data[n, channel, i-padding, j-padding]
-            '''
-            return square
-
-        def to_column(square):
-            return np.reshape(square, -1)
+                        X_col[n,row,loc] = 0
+        return X_col
 
 
-        for n in range(data.shape[0]):
-            for channel in range(data.shape[1]):
-                #THE GENERATOR IS THE PROBLEM
-                locations = [loc for loc in ConvLayer.location_generator(height, width, kernel_size, padding, stride)]
-                for index, (row, col) in enumerate(locations):
-                    X_col[n, channel*kernel_size*kernel_size:(channel+1)*kernel_size*kernel_size, index] = to_column(get_square(row, col))
+    def im_2_col_indexing(self, data):
+        size = self.kernel_size
+        C = self.input_channels
+        padding = self.padding
+        data = np.pad(data,((0,0),(0,0),(padding,padding),(padding,padding)))
+
+        locations = [loc for loc in ConvLayer.location_generator(self.height,
+                                                                 self.width,
+                                                                 size,
+                                                                 padding,
+                                                                 self.stride)]
+
+        def channel_square_indices(row, col):
+            # in a given column of X_col, corresponding to a location
+            # return a column of indices in padded_data (c, h,k)
+            # channel_square_indices(row,col)[3] is entry in third row at loc
+            k = self.kernel_size
+            c = self.input_channels
+            return [(channel, x, y) for channel in range(c)
+                                    for (x,y) in product(range(row, row+k),
+                                                         range(col,col+k))]
+        index_map = []                              
+        for loc, (row,col) in enumerate(locations):
+            index_map.append([])
+            for channel_square_index in channel_square_indices(row, col):
+                index_map[loc].append(channel_square_index)
+        
+
+        X_col[:, r, loc] = padded_data[:, ]
+
+
+
     
     def im_2_col_no_sq(self, data):
         '''
         See im_2_col. This method from [Zhang '17]
 
-        currently fails for stride != 1
-        paper does not deal with this. I just used my H1 and W1
+        Implementation and stride count.
+        Next step is split up the k loop for parallel computation
         '''
         H0 = self.height
         H1 = self.output_height
@@ -95,16 +156,17 @@ class ConvLayer(Layer):
         D0 = self.input_channels
         D1 = self.output_channels
         P = self.padding
-        F = self.kernel_size
+        K = self.kernel_size
+        S = self.stride
         X = data
-        for k in range(D0*F*F*H1*W1):
+        for k in range(D0*K*K*H1*W1):
             p = k // (H1*W1)
             q = k % (H1*W1)
-            d0 = (p//F)//F
-            i0 = q//W1 + (p//F) %F
-            j0 = q % W1 + p % F
+            d0 = (p//K)//K
+            i0 = S*(q//W1) + (p//K) %K
+            j0 = S*(q % W1) + p % K
             if (i0 >= P and j0 >= P and i0 < H0 + P and j0 < W0 + P):
-                self.X_col[:,p,q] = data[:,d0,i0-P,j0-P]
+                self.X_col[:,p,q] = X[:,d0,i0-P,j0-P]
             else:
                 self.X_col[:,p,q] = 0
 
@@ -115,38 +177,22 @@ class ConvLayer(Layer):
         '''
         See im_2_col. This method pads the data first instead of relegating to get square
         '''
-        def get_square(row, col):
-            return data[n, channel, row:row + self.kernel_size,
-                                    col:col + self.kernel_size]
-
-        def to_column(square):
-            return np.reshape(square, -1)
-
-        def column_square(row, col):
-            return data[n, channel, row:row + self.kernel_size,
-                                    col:col + self.kernel_size].reshape(-1)
-
         size = self.kernel_size
         padding = self.padding
         data = np.pad(data,((0,0),(0,0),(padding,padding),(padding,padding)))
 
-
+        # saves a whole second to do this outside instead of using a generator
+        # I guess locations don't take that much space.
         locations = [loc for loc in ConvLayer.location_generator(self.height,
                                                                  self.width,
-                                                                 self.kernel_size,
-                                                                 self.padding,
+                                                                 size,
+                                                                 padding,
                                                                  self.stride)]
 
         for n in range(data.shape[0]):
             for channel in range(data.shape[1]):
-                '''
-                locations = ConvLayer.location_generator(self.height,
-                                                         self.width,
-                                                         self.kernel_size,
-                                                         self.padding,
-                                                         self.stride)
-                '''
                 for index, (row, col) in enumerate(locations):
+                    #HOW TO DO THIS WITHOUT RESHAPING EVERY TIME
                     self.X_col[
                                n,
                                channel*size*size:(channel+1)*size*size,
@@ -155,9 +201,9 @@ class ConvLayer(Layer):
                                = data[
                                       n,
                                       channel,
-                                      row:row + self.kernel_size,
-                                      col:col + self.kernel_size
-                                     ].reshape(-1)
+                                      row:row + size,
+                                      col:col + size
+                                     ].reshape(size*size)
 
 
     def im_2_col(self, data):
@@ -208,6 +254,22 @@ class ConvLayer(Layer):
                 y = 0
                 x += stride
 
+    @staticmethod
+    def location_generator2(height, width, kernel_size, padding, stride):
+            ''' Generator for kernel placements on padded data
+
+            :return: (x,y) row column location of top left corner of kernel
+            '''
+            x = 0
+            y = 0
+            while ((x + kernel_size - 1) < (height + 2*padding)):
+                while((y + kernel_size - 1) < (width + 2*padding)):
+                    yield [x,y]
+                    y += stride
+                y = 0
+                x += stride
+
+
     def col_2_im(self, data_col, height, width):
         '''
         Used in backward pass
@@ -226,7 +288,11 @@ class ConvLayer(Layer):
 
         padded_image_grad = np.zeros((data_col.shape[0], self.input_channels, self.height+2*self.padding, self.width+2*self.padding))
         
-        kernel_locations = ConvLayer.location_generator(self.height, self.width, self.kernel_size, self.padding, self.stride)
+        kernel_locations = [loc for loc in ConvLayer.location_generator(self.height,
+                                                                        self.width,
+                                                                        self.kernel_size,
+                                                                        self.padding,
+                                                                        self.stride)]
         for index, (row, col) in enumerate(kernel_locations):
             padded_image_grad[
                               :,
@@ -237,8 +303,9 @@ class ConvLayer(Layer):
         
         
         if self.padding == 0:
-            return padded_image_grad[:, :, :, :]
-        return padded_image_grad[:, :, self.padding:-self.padding, self.padding:-self.padding]
+            return padded_image_grad
+        else:
+            return padded_image_grad[:, :, self.padding:-self.padding, self.padding:-self.padding]
 
     def number_of_locations(self, height, width, kernel_size, padding, stride):
         horz_loc = 1 + ((width + 2*padding - kernel_size)//stride)
@@ -262,23 +329,46 @@ class ConvLayer(Layer):
         self.width = data.shape[3]
 
         size_size_channels = self.kernel_size*self.kernel_size*self.input_channels
+
+        #also initializes output height and width
         self.location_count = self.number_of_locations(self.height, self.width, self.kernel_size, self.padding, self.stride)
 
         self.X_col = np.zeros((data.shape[0], size_size_channels, self.location_count), dtype = np.float32)
         
+        ###########################
+        # IM2COL GOES HERE
+        ###########################
+
         #Original line:
         #self.im_2_col(data)
 
+        #self.im_2_col_no_sq(data)
+      
         # With numpy padding:
-        self.im_2_col_pad(data)
-
-        # fancy, stride 1
-        # self.im_2_col_no_sq(data)
-
-        #failing numba code
-        #self.X_col = ConvLayer.numba_im_2_col(data,self.kernel_size,self.padding,self.height,self.width,self.X_col)
+        # self.im_2_col_pad(data)
         
-        W_row = np.reshape(np.swapaxes(self.weight.data,0,1), (self.weight.data.shape[1],-1))
+
+        # other fance
+        #self.im_2_col_indexing(data)
+
+        
+        #numba code
+        self.X_col = ConvLayer.numba_im_2_col_2(data,
+                                              self.height,
+                                              self.output_height,
+                                              self.width,
+                                              self.output_width,
+                                              self.input_channels,
+                                              self.output_channels,
+                                              self.padding,
+                                              self.kernel_size,
+                                              self.stride)
+        
+        
+
+        ###########################
+
+        W_row = np.reshape(np.swapaxes(self.weight.data,0,1), (self.weight.data.shape[1],size_size_channels))
         Y_col = W_row @ self.X_col
         return Y_col.reshape((*Y_col.shape[0:2], self.output_height, self.output_width)) + self.bias.data.reshape((1,self.output_channels,1,1))
 
@@ -304,6 +394,7 @@ class ConvLayer(Layer):
 
         W_row = np.reshape(np.swapaxes(self.weight.data,0,1), (self.weight.data.shape[1],-1))
         previous_grad_col = previous_partial_gradient.reshape((*previous_partial_gradient.shape[0:2],-1))
+
         next_grad_col = W_row.T @ previous_grad_col
 
         return self.col_2_im(next_grad_col, self.height, self.width)
